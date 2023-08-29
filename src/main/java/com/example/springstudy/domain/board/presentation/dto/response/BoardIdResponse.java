d0b7a8c2713382eaf38ac2e67c2119b5c94795df
soyeonkim0227 <soyeonkim0227@naver.com>
2023-08-29 18:55:16 +0900

    👀 :: response 수정

---
+++ src/main/java/com/example/springstudy/domain/board/presentation/dto/response/BoardIdResponse.java
@@ -0,0 +1,11 @@
+package com.example.springstudy.domain.board.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BoardIdResponse {
+
+    private Long boardId;
+}
