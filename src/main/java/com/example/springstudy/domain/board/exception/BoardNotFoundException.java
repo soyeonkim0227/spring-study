ec8a3fb5d801080b900c4ff1fc29484168bd4ff3
soyeonkim0227 <soyeonkim0227@naver.com>
2023-08-30 12:25:36 +0900

    ✨ :: board 예외처리 추가

---
+++ src/main/java/com/example/springstudy/domain/board/exception/BoardNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.springstudy.domain.board.exception;
+
+import com.example.springstudy.global.error.CustomException;
+import com.example.springstudy.global.error.ErrorCode;
+
+public class BoardNotFoundException extends CustomException {
+
+    public static final CustomException EXCEPTION = new BoardNotFoundException();
+
+    private BoardNotFoundException() {
+        super(ErrorCode.BOARD_NOT_FOUND);
+    }
+}
