41a32be788f872e814cafedb4e68fde44d293583
soyeonkim0227 <soyeonkim0227@naver.com>
2023-09-09 23:45:27 +0900

    👀 :: annotation 수정

--- src/main/java/com/example/springstudy/global/error/CustomException.java
+++ src/main/java/com/example/springstudy/global/error/CustomException.java
@@ -1,10 +1,11 @@
 package com.example.springstudy.global.error;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class CustomException extends RuntimeException {
 
     private final ErrorCode errorCode;
